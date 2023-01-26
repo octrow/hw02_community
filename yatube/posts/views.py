ef24134a111f9b7c7913e04c31cecce45ecaf9f9
Daniil Petrov <octrow@yandex.ru>
2023-01-27 00:46:16 +0600

    Проект на ревью v.4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,12 +6,8 @@ from .models import Group, Post
 
 def index(request):
     template = 'posts/index.html'
-    title = 'Последние обновления на сайте'
-    text = 'Welcome to YaTube!'
     posts = Post.objects.all()[:settings.NUM_POSTS]
     context = {
-        'title': title,
-        'text': text,
         'posts': posts
     }
     return render(request, template, context)
@@ -20,11 +16,9 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    title = group.title
     posts = Post.objects.filter(group=group)[:settings.NUM_POSTS]
     context = {
         'group': group,
         'posts': posts,
-        'title': title
     }
     return render(request, template, context)
